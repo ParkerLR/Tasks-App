3ab00df329f19b7757fd5c12f53d84c4df4231dd
ParkerLR <110330754+ParkerLR@users.noreply.github.com>
2023-07-15 10:58:28 -0600

    Actualizar app.js
    solo agregue comentario, en un rato lo tengo que entender

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 
-    console.log('HOLA')
+    console.log('HOLA') \\ Mensaje de hola, manda un mensaje en consola que diaga hola
 
-    $('#search').keyup(function() {
+    $('#search').keyup(function() \\Solo indica que usa la variable $search (recuaro de busqueda) 
         let search = $('#search').val();
    
-        $.ajax({
-            url: 'tasks-search.php',
+        $.ajax({											\\utiliza el metodo ajax para para bo tener que refrescar la paguina.														
+            url: 'tasks-search.php',		\\indica que este js usa el archivo tasks-sear,php para hacer la busqueda 
             type: 'POST',
             data: { search },
              success: function(response){
@@ -19,4 +19,6 @@ $(document).ready(function() {
     }) 
     
 });
+
+
 
