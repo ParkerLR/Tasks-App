6b1e6de6d73fef336393a01baccbef84bf43647c
Alan Alozano <alan_sito2013@hotmail.com>
2023-08-09 00:05:49 -0600

    agregue el cierre de llaves

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    console.log('HOLA'); // Mensaje de hola, manda un mensaje en consola que diga hola
 
-    console.log('HOLA') \\ Mensaje de hola, manda un mensaje en consola que diaga hola
-
-    $('#search').keyup(function() \\Solo indica que usa la variable $search (recuaro de busqueda) 
+    $('#search').keyup(function () { // Corrección: Agrega la apertura de llaves aquí
         let search = $('#search').val();
-   
-        $.ajax({											\\utiliza el metodo ajax para para bo tener que refrescar la paguina.														
-            url: 'tasks-search.php',		\\indica que este js usa el archivo tasks-sear,php para hacer la busqueda 
+
+        $.ajax({
+            // Utiliza el método ajax para no tener que refrescar la página.
+            url: 'tasks-search.php', // Indica que este js usa el archivo tasks-search.php para hacer la búsqueda.
             type: 'POST',
             data: { search },
-             success: function(response){
+            success: function (response) {
                 console.log(response);
-                 
-             }
-             
-        })
-    }) 
-    
+            }
+        });
+    }); // Corrección: Agrega el cierre de llaves aquí
+
 });
-
-
-
